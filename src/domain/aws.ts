7474fdae8b30c4da8aec6200eca58e98775928e3
Brian Foody <brianfoody@gmail.com>
2022-08-03 15:59:20 +0800

    Fix ty[es

--- src/domain/aws.ts
+++ src/domain/aws.ts
@@ -5,27 +5,6 @@ export type AWSCredentials = Credentials & {
   identityId?: string;
 };
 
-export class UnauthorisedError extends Error {
-  constructor(msg: string) {
-    super(msg);
-
-    this.name = "UnauthorisedError";
-
-    // Set the prototype explicitly.
-    Object.setPrototypeOf(this, UnauthorisedError.prototype);
-  }
-}
-export class InvalidConfigurationError extends Error {
-  constructor(msg: string) {
-    super(msg);
-
-    this.name = "InvalidConfigurationError";
-
-    // Set the prototype explicitly.
-    Object.setPrototypeOf(this, InvalidConfigurationError.prototype);
-  }
-}
-
 export type AwsRegion =
   | "us-east-1"
   | "us-east-2"
@@ -46,26 +25,6 @@ export type AwsRegion =
   | "eu-west-2";
 
 // SSO
-export type AwsSsoConfig = {
-  alias: string;
-  sso_start_url: string;
-  sso_region: string;
-  sso_account_id: string;
-  sso_role_name: string;
-  region?: string;
-};
-
-export type PermissionSet = {
-  accountId: string;
-  name: string;
-};
-
-export type Account = {
-  accountId: string;
-  name?: string;
-  defaultRegion?: string;
-  roles: string[];
-};
 
 export type SSOSession = {
   startUrl: string;
@@ -74,26 +33,7 @@ export type SSOSession = {
   expiresAt: Date;
 };
 
-export type Access = {
-  organisations: Organisation[];
-};
-
-export type Organisation = {
-  logicallyDeleted?: boolean;
-  ssoStartUrl: string;
-  ssoRegion: string;
-  nickname?: string; // added by user
-  accounts: Account[];
-  roles: string[];
-  accountsSyncedAtIso?: string;
-  authorisedUntil?: Date;
-};
-
 export type AccessPair = {
   accountId: string;
   permissionSet: string;
-};
-
-export type AccessCard = AccessPair & {
-  region: AwsRegion;
 };
